9133f4942349b83d8a08c3e3ba4d577f0449ba6e
JASON0916 <JASON0916PHOENIX@GMAIL.COM>
2015-07-05 00:01:53 +0800

    bitmap algorithm

--- algorithm/bitmap.py
+++ algorithm/bitmap.py
@@ -8,21 +8,22 @@ space: O(1)
 
 class Solution:
     # @param nums: a list of ints
-    # @return a list of ints without duplicates
+    # @return a list of ints withou\t duplicates
     def delete_duplicate(self, nums):
         length = int(max(nums)/32)+1
         num_list = [0]*length
         i = 0
         bitmask = 0x1F
         while i < len(nums):
-            indicy1, indicy2 = int(nums[i]/32), nums[i]%32
-            if (num_list[indicy1] >> indicy2) & bitmask == 1:
+            indicy1, indicy2 = int(nums[i]/32), nums[i] % 32
+            bit = 1 << indicy2
+            if bit & num_list[indicy1]:
                 nums.pop(i)
-            elif (num_list[indicy1] >> indicy2) & bitmask == 0:
-                num_list[indicy1] |= 0x01 << indicy2
+            else:
+                num_list[indicy1] |= 1 << indicy2
                 i += 1
         return nums
 
 
 if __name__ == '__main__':
-    print(Solution().delete_duplicate([1,11,1,1,5]))
+    print(Solution().delete_duplicate([1,11,1,1,1,1,5,1,5,5,5,5,]))
